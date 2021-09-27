a659e0722bcf965bc1fa16fdcbea085b1055d542
Sam Rodrigue <samrodrigue@hotmail.com>
2021-09-27 13:09:04 -0400

    Remove i2c library and initializations.
    Attempting to refine some functions as this will likely become
    a library for future projects.

--- keypad.c
+++ keypad.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/i2c.h"
-
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 4
-#define I2C_SCL 5
 
 // Keypad defines
 #define KEY_IN_SIZE 4
@@ -48,21 +40,20 @@ static char const keys_alt[KEY_IN_SIZE][KEY_OUT_SIZE] = {
 	'=', '\r', '\n'
 };
 
-static uint32_t input_state_data[2][KEY_OUT_SIZE];
-static bool input_state_swapped;
-static uint32_t *input_state;
-static uint32_t *input_state_previous;
+static uint32_t keypad_input_state[KEY_OUT_SIZE] = { 0, 0, 0 };
 static bool alt_key_printable;
 
 #define ALT_COL 0
 #define ALT_ROW 3
 
-bool alt_held()
+
+bool keypad_is_alt_held()
 {
-	return input_state[0] & (1ul << key_in[3]);
+	return keypad_input_state[ALT_COL] & (1ul << key_in[ALT_ROW]);
 }
 
-bool is_alt_key(size_t row, size_t col)
+
+bool keypad_is_alt_key(size_t row, size_t col)
 {
 	return row == ALT_ROW && col == ALT_COL;
 }
@@ -71,6 +62,7 @@ bool is_alt_key(size_t row, size_t col)
 #define time_off() sleep_ms(1)
 #define time_on() sleep_ms(1)
 
+
 bool keypad_process(uint32_t input, size_t col)
 {
 	for (size_t row = 0; row < KEY_IN_SIZE; ++row)
@@ -78,17 +70,17 @@ bool keypad_process(uint32_t input, size_t col)
 		uint32_t mask = 1ul << key_in[row];
 
 		// Event Pressed
-		if (~input_state[col] & input & mask)
+		if (~keypad_input_state[col] & input & mask)
 		{
 			// Alt key
-			if (is_alt_key(row, col))
+			if (keypad_is_alt_key(row, col))
 			{
 				alt_key_printable = true;
 				continue;
 			}
 
 			// Is alt key currently held
-			if (alt_held(input_state))
+			if (keypad_is_alt_held(keypad_input_state))
 			{
 				alt_key_printable = false;
 				putchar(keys_alt[row][col]);
@@ -98,10 +90,10 @@ bool keypad_process(uint32_t input, size_t col)
 		}
 
 		// Event Release Key
-		else if (input_state[col] & ~input & mask)
+		else if (keypad_input_state[col] & ~input & mask)
 		{
 			// If alt is released
-			if (is_alt_key(row, col))
+			if (keypad_is_alt_key(row, col))
 			{
 				if (alt_key_printable)
 					putchar(keys[row][col]);
@@ -113,19 +105,17 @@ bool keypad_process(uint32_t input, size_t col)
 
 void keypad_task()
 {
-	bool cycle_input_state = false;
 	for (size_t col = 0; col < KEY_OUT_SIZE; ++col)
 	{
 		time_off();
 		gpio_put(key_out[col], 0);
 		time_on();
 
-		uint32_t keypad_in_state = ~gpio_get_all() & KEY_IN;
-		if (input_state[col] != keypad_in_state)
+		uint32_t input_state = ~gpio_get_all() & KEY_IN;
+		if (keypad_input_state[col] != input_state)
 		{
-			cycle_input_state = true;
-			keypad_process(keypad_in_state, col);
-			input_state[col] = keypad_in_state;
+			keypad_process(input_state, col);
+			keypad_input_state[col] = input_state;
 		}
 
 		gpio_put(key_out[col], 1);
@@ -133,51 +123,32 @@ void keypad_task()
 }
 
 
+void gpio_set_pull_up_masked(uint32_t gpio_mask)
+{
+	for(uint gpio = 0; gpio < 32; ++gpio) {
+		if (gpio_mask & 1)
+			gpio_pull_up(gpio);
+
+		gpio_mask >>= 1;
+	}
+}
+
+
+void keypad_init()
+{
+	// GPIO
+	gpio_init_mask(KEY_IN | KEY_OUT);
+	gpio_set_dir_in_masked(KEY_IN);
+	gpio_set_dir_out_masked(KEY_OUT);
+	gpio_set_pull_up_masked(KEY_IN);
+	gpio_put_masked(KEY_OUT, 1);
+}
+
+
 int main()
 {
 	stdio_init_all();
-
-	// I2C Initialisation. Using it at 400Khz.
-	i2c_init(I2C_PORT, 400*1000);
-
-	gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-	gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-	gpio_pull_up(I2C_SDA);
-	gpio_pull_up(I2C_SCL);
-
-	// Keypad GPIO
-	gpio_init(KEY_IN_0);
-	gpio_init(KEY_IN_1);
-	gpio_init(KEY_IN_2);
-	gpio_init(KEY_IN_3);
-	gpio_set_dir(KEY_IN_0, GPIO_IN);
-	gpio_set_dir(KEY_IN_1, GPIO_IN);
-	gpio_set_dir(KEY_IN_2, GPIO_IN);
-	gpio_set_dir(KEY_IN_3, GPIO_IN);
-	gpio_pull_up(KEY_IN_0);
-	gpio_pull_up(KEY_IN_1);
-	gpio_pull_up(KEY_IN_2);
-	gpio_pull_up(KEY_IN_3);
-
-	// Configure GPIO IRQ
-	// gpio_set_irq_enabled_with_callback(KEY_IN_0, GPIO_IRQ_EDGE_FALL, true, &keypad_event);
-	// gpio_set_irq_enabled(KEY_IN_1, GPIO_IRQ_EDGE_FALL, true);
-	// gpio_set_irq_enabled(KEY_IN_2, GPIO_IRQ_EDGE_FALL, true);
-	// gpio_set_irq_enabled(KEY_IN_3, GPIO_IRQ_EDGE_FALL, true);
-
-	gpio_init(KEY_OUT_0);
-	gpio_init(KEY_OUT_1);
-	gpio_init(KEY_OUT_2);
-	gpio_set_dir(KEY_OUT_0, GPIO_OUT);
-	gpio_set_dir(KEY_OUT_1, GPIO_OUT);
-	gpio_set_dir(KEY_OUT_2, GPIO_OUT);
-	gpio_put(KEY_OUT_0, 1);
-	gpio_put(KEY_OUT_1, 1);
-	gpio_put(KEY_OUT_2, 1);
-
-	input_state_swapped = false;
-	input_state = input_state_data[0];
-	input_state_previous = input_state_data[1];
+	keypad_init();
 
 	while (1)
 	{
