e4c13c224b247237dc3ea257ea8bf8e57a06af53
Sam Rodrigue <samrodrigue@hotmail.com>
2021-10-04 23:02:25 -0400

    Keypad is configured using macros
    Macros KEYPAD_IN and KEYPAD_OUT must be defined as csv of gpio
    index. Other macros such as mask and size are now derived.
    Active high and active low can be configured with KEYPAD_LEVEL_ON.

--- keypad.h
+++ keypad.h
@@ -2,45 +2,49 @@
 #define _KEYPAD_H_
 
 #include "pico/stdlib.h"
+#include <stdio.h>
+#define passert(check, message) if (!(check)) { printf("%s:%d %s", __FILE__, __LINE__, message); assert(false); }
 
-// Keypad configuration
-#define KEY_IN_SIZE 4
-#define KEY_IN_0 9
-#define KEY_IN_1 10
-#define KEY_IN_2 11
-#define KEY_IN_3 12
+#ifndef KEYPAD_OUT
+   #error KEYPAD_OUT must be defined example: #define KEYPAD_OUT 1, 2, 3
+#endif
+#ifndef KEYPAD_IN
+   #error KEYPAD_IN must be defined example: #define KEYPAD_IN 4, 5
+#endif
 
-#define KEY_OUT_SIZE 3
-#define KEY_OUT_0 13
-#define KEY_OUT_1 14
-#define KEY_OUT_2 15
+#define KEYPAD_LEVEL_ON 1
+#define KEYPAD_LEVEL_OFF !KEYPAD_LEVEL_ON
 
-#define KEY_IN_MASK \
-	((1ul << KEY_IN_0) | \
-	 (1ul << KEY_IN_1) | \
-	 (1ul << KEY_IN_2) | \
-	 (1ul << KEY_IN_3))
+#define KEYPAD_COUNT_N(_0, _1, _2, _3, _4, _5, _6, _7, N, ...) N
+#define KEYPAD_COUNT(...) KEYPAD_COUNT_N(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1)
 
-#define KEY_OUT_MASK \
-	((1ul << KEY_OUT_0) | \
-	 (1ul << KEY_OUT_1) | \
-	 (1ul << KEY_OUT_2))
+#define KEYPAD_MASK_1(_1) (1ul << _1)
+#define KEYPAD_MASK_2(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_1(__VA_ARGS__)
+#define KEYPAD_MASK_3(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_2(__VA_ARGS__)
+#define KEYPAD_MASK_4(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_3(__VA_ARGS__)
+#define KEYPAD_MASK_5(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_4(__VA_ARGS__)
+#define KEYPAD_MASK_6(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_5(__VA_ARGS__)
+#define KEYPAD_MASK_7(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_6(__VA_ARGS__)
+#define KEYPAD_MASK_8(_1, ...) KEYPAD_MASK_1(_1) | KEYPAD_MASK_7(__VA_ARGS__)
+#define KEYPAD_MASK(...) (KEYPAD_COUNT_N(__VA_ARGS__, \
+	                                KEYPAD_MASK_8, KEYPAD_MASK_7, KEYPAD_MASK_6, KEYPAD_MASK_5, \
+	                                KEYPAD_MASK_4, KEYPAD_MASK_3, KEYPAD_MASK_2, KEYPAD_MASK_1)(__VA_ARGS__))
 
-#define KEY_ALT_COL 0
-#define KEY_ALT_ROW 3
+#define KEYPAD_IN_SIZE KEYPAD_COUNT(KEYPAD_IN)
+#define KEYPAD_OUT_SIZE KEYPAD_COUNT(KEYPAD_OUT)
 
-#define KEYPAD_MAX_EVENT_SIZE 4
+#define KEYPAD_IN_MASK KEYPAD_MASK(KEYPAD_IN)
+#define KEYPAD_OUT_MASK KEYPAD_MASK(KEYPAD_OUT)
 
-static uint32_t const key_in[KEY_IN_SIZE] = { KEY_IN_3, KEY_IN_2, KEY_IN_1, KEY_IN_0 };
-static uint32_t const key_out[KEY_OUT_SIZE] = { KEY_OUT_2, KEY_OUT_1, KEY_OUT_0 };
+static uint32_t const keypad_in[] = { KEYPAD_IN };
+static uint32_t const keypad_out[] = { KEYPAD_OUT };
 
-static uint32_t keypad_input_state[KEY_OUT_SIZE] = { 0, 0, 0 };
+static uint32_t keypad_input_state[KEYPAD_OUT_SIZE] = { 0 };
 
 enum event_type
 {
 	KEY_RELEASE,
-	KEY_PRESSED,
-	NONE
+	KEY_PRESSED
 };
 
 struct keypad_event
@@ -50,16 +54,72 @@ struct keypad_event
 	uint8_t row;
 };
 
-struct keypad_event_queue
+
+#define time_off() sleep_ms(1)
+#define time_on() sleep_ms(1)
+
+void keypad_task(void (*action)(struct keypad_event))
 {
-	uint8_t size;
-	struct keypad_event events[KEYPAD_MAX_EVENT_SIZE];
-};
+	for (uint8_t col = 0; col < KEYPAD_OUT_SIZE; ++col)
+	{
+		time_off();
+		gpio_put(keypad_out[col], KEYPAD_LEVEL_ON);
+		time_on();
 
-void keypad_init();
+		uint32_t input_state = ((KEYPAD_LEVEL_ON == 1) ? gpio_get_all() : ~gpio_get_all()) & KEYPAD_IN_MASK;
+		if (keypad_input_state[col] != input_state)
+		{
+			for (uint8_t row = 0; row < KEYPAD_IN_SIZE; ++row)
+			{
+				uint32_t mask = 1ul << keypad_in[row];
 
-void keypad_task(struct keypad_event_queue *event_queue);
+				uint32_t previous_state = keypad_input_state[col] & mask;
+				uint32_t current_state = input_state & mask;
 
-bool keypad_get(uint8_t row, uint8_t col);
+				if (previous_state == current_state)
+					continue;
+
+				keypad_input_state[col] ^= mask;
+				
+				struct keypad_event event = {
+					current_state == 0ul ? KEY_RELEASE : KEY_PRESSED,
+					col, row };
+
+				action(event);
+			}
+		}
+
+		gpio_put(keypad_out[col], KEYPAD_LEVEL_OFF);
+	}
+}
+
+
+bool keypad_get(uint8_t row, uint8_t col)
+{
+	passert(row < KEYPAD_IN_SIZE && col < KEYPAD_OUT_SIZE, "keypad_get out of range");
+	return keypad_input_state[col] & (1ul << keypad_in[row]);
+}
+
+
+static void _gpio_set_pull_masked(uint32_t gpio_mask)
+{
+	for(uint gpio = 0; gpio < 32; ++gpio) {
+		if (gpio_mask & 1ul)
+			gpio_set_pulls(gpio, KEYPAD_LEVEL_ON == 0, KEYPAD_LEVEL_ON == 1);
+
+		gpio_mask >>= 1;
+	}
+}
+
+
+void keypad_init()
+{
+	// GPIO
+	gpio_init_mask(KEYPAD_IN_MASK | KEYPAD_OUT_MASK);
+	gpio_set_dir_in_masked(KEYPAD_IN_MASK);
+	gpio_set_dir_out_masked(KEYPAD_OUT_MASK);
+   _gpio_set_pull_masked(KEYPAD_IN_MASK);
+	gpio_put_masked(KEYPAD_OUT_MASK, (KEYPAD_LEVEL_ON == 1) ? 0x0ul : ~0x0ul);
+}
 
 #endif
--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+// Keypad GPIO index and order
+#define KEYPAD_IN 12, 13, 14, 15
+#define KEYPAD_OUT 9, 10, 11
 #include "keypad.h"
 
 #include <stdio.h>
@@ -5,73 +8,75 @@
 
 #define SCREEN_SIZE 16
 
-static char const keys[KEY_IN_SIZE][KEY_OUT_SIZE] = {
+static char const keys[KEYPAD_IN_SIZE][KEYPAD_OUT_SIZE] = {
 	'1', '2', '3',
 	'4', '5', '6',
 	'7', '8', '9',
 	'+', '0', '-'
 };
 
-static char const keys_alt[KEY_IN_SIZE][KEY_OUT_SIZE] = {
+static char const keys_alt[KEYPAD_IN_SIZE][KEYPAD_OUT_SIZE] = {
 	'a', '\t', '\b',
 	'd', '\v', '\a',
 	'g', '\0', '\f',
 	'=', '\r', '\n'
 };
 
+static char screen[SCREEN_SIZE];
+static uint8_t cursor;
+
+
+void print_keypad()
+{
+	putchar('\f');
+	for (uint8_t row = 0; row < KEYPAD_IN_SIZE; ++row)
+	{
+		for (uint8_t col = 0; col < KEYPAD_OUT_SIZE; ++col)
+		{
+			putchar(' ');
+			if (keypad_get(row, col))
+				putchar(keys[row][col]);
+			else
+				putchar('-');
+		}
+		putchar('\n');
+	}
+}
+
+// Action to perform on a keypad event
+void key_action(struct keypad_event event)
+{
+	print_keypad();
+
+	switch (event.type)
+	{
+	case KEY_RELEASE:
+		break;
+
+	case KEY_PRESSED:
+		if (++cursor == SCREEN_SIZE)
+			cursor = 0;
+		screen[cursor] = keys[event.row][event.col];
+		break;
+	}
+
+	for (uint8_t i = 0; i <= cursor; ++i)
+		putchar(screen[i]);
+}
+
+
 int main()
 {
 	stdio_init_all();
 	keypad_init();
+	print_keypad();
 
-	struct keypad_event_queue event_queue;
-	struct keypad_event *event;
-	char screen[SCREEN_SIZE];
-	uint8_t pos = 0;
-
+	cursor = SCREEN_SIZE - 1;
 	memset(screen, '\0', SCREEN_SIZE);
 
 	while (1)
 	{
-		keypad_task(&event_queue);
-		uint8_t event_size = event_queue.size;
-		while (event_queue.size > 0)
-		{
-			--event_queue.size;
-			event = &event_queue.events[event_queue.size];
-
-			putchar('\f');
-			for (uint8_t row = 0; row < KEY_IN_SIZE; ++row)
-			{
-				for (uint8_t col = 0; col < KEY_OUT_SIZE; ++col)
-				{
-					putchar(' ');
-					if (keypad_get(row, col))
-						putchar(keys[row][col]);
-					else
-						putchar('-');
-				}
-				putchar('\n');
-			}
-
-			switch (event->type)
-			{
-			case KEY_RELEASE:
-				break;
-
-			case KEY_PRESSED:
-				screen[pos++] = keys[event->row][event->col];
-				// putchar(keys[event->row][event->col]);
-				break;
-
-			default:
-				break;
-			}
-			printf("events: %d\n", event_size);
-
-			for (uint8_t i = 0; i < pos; ++i)
-				putchar(screen[i]);
-		}
+		keypad_task(&key_action);
 	}
 
 	return 0;
